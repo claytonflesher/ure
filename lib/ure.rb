d334b1a0f9c58cf143404c259435bafe1eab3b74
Calvyn82 <claytonflesher@gmail.com>
2015-12-03 07:55:30 -0600

    Added Rdoc documentation. Fixed link to Code of Conduct. Ran code of conduct node build.

--- lib/ure.rb
+++ lib/ure.rb
@@ -1,11 +1,18 @@
 require "ure/version"
 require "json"
+##
+# Immutable alternative to Struct that requires keyword arguments
 class Ure < BasicObject
   include ::Enumerable
+
+  ##
+  # Returns the ure members as an array of symbols.
   def self.members
     @members.freeze
   end
 
+  ##
+  # Create an ure named by its constant.
   def self.new(*members, &body)
     ::Class.new(self) do
       instance_variable_set(:@members, members)
@@ -58,6 +65,8 @@ class Ure < BasicObject
 
   attr_reader :fields
 
+  ##
+  # Equality---Returns true if other has the same ure subclass and has equal member values.
   def ==(arg)
     if self.class == arg.class
       fields == arg.fields
@@ -68,38 +77,56 @@ class Ure < BasicObject
     false
   end
 
+  ##
+  # Because Ure doesn't care about indexing, this allows users to treat instances of Ure as a hash.
   def [](key)
     fields[key]
   end
 
+  ##
+  # Converts the fields into a hash and calls each on them.
   def each(&block)
     to_h.each(&block)
   end
 
+  ##
+  # Returns a string describing the object and its fields
   def to_s
     "#<#{::Ure} #{self.class} #{fields.to_s}"
   end
 
+  ##
+  # Returns a string describing the object and its fields
   def inspect
     "#<#{::Ure} #{self.class} #{fields.to_s}"
   end
 
+  ##
+  # Alias for #values
   def to_a
     fields.values
   end
 
+  ##
+  # Alias for #fields
   def to_h
     fields.to_h
   end
 
+  ##
+  # Returns an array of the values in the fields
   def values
     fields.values
   end
 
+  ##
+  # Takes one or more keys as arguments, and returns an array of the corresponding values.
   def values_at(name, *args)
     [fields[name]] + args.map { |arg| fields[arg] }
   end
 
+  ##
+  # Returns a JSON string of members and values.
   def to_json
     fields.to_json
   end
--- lib/ure/version.rb
+++ lib/ure/version.rb
@@ -1,3 +1,3 @@
 class Ure < BasicObject
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
